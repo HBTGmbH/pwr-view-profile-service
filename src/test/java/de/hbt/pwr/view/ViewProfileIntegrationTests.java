df5e2e07af3bb74eeabe2ed057fa07ed461a7c0e
nt <nt@hbt.de>
2017-09-07 16:30:25 +0200

    NOJIRA added a small integrative test that validates that persistence works as intended

---
+++ src/test/java/de/hbt/pwr/view/ViewProfileIntegrationTests.java
@@ -0,0 +1,97 @@
+package de.hbt.pwr.view;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.hbt.pwr.view.client.profile.ProfileServiceClient;
+import de.hbt.pwr.view.client.profile.model.Profile;
+import de.hbt.pwr.view.model.ViewProfile;
+import de.hbt.pwr.view.service.ViewProfileImporter;
+import de.hbt.pwr.view.service.ViewProfileService;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import redis.embedded.RedisServer;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
+/**
+ *
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@RunWith(SpringJUnit4ClassRunner.class)
+@ActiveProfiles("test")
+@TestPropertySource(properties = {"spring.redis.port = " + ViewProfileIntegrationTests.TEST_REDIS_PORT_STRING, })
+public class ViewProfileIntegrationTests {
+
+
+    public static final Integer TEST_REDIS_PORT_INT = 24312;
+    public static final String TEST_REDIS_PORT_STRING = "24312";
+
+    private static RedisServer redisServer;
+
+    @Autowired
+    private ViewProfileImporter viewProfileImporter;
+
+    @Autowired
+    private ViewProfileService viewProfileService;
+
+    @MockBean
+    private ProfileServiceClient profileServiceClient;
+
+    private final String initials = "tst";
+
+    private ViewProfile viewProfile;
+
+    @BeforeClass
+    public static void setUpRedis() throws IOException {
+        redisServer = RedisServer.builder()
+                .port(TEST_REDIS_PORT_INT)
+                //.redisExecProvider(customRedisExec) //com.github.kstyrc (not com.orange.redis-embedded)
+                .setting("maxmemory 128M") //maxheap 128M
+                .build();
+
+    }
+
+    @Before
+    public void startRedis() {
+        redisServer.start();
+    }
+
+
+    @After
+    public void stopRedis() {
+        redisServer.stop();
+    }
+
+    @Test
+    public void contextLoads() {
+
+    }
+
+    private void addTestData() throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.findAndRegisterModules();
+        InputStream profileAsInputStream = this.getClass().getClassLoader().getResourceAsStream("test_profile_1.json");
+        Profile profile = objectMapper.readValue(profileAsInputStream, Profile.class);
+        given(profileServiceClient.getSingleProfile(initials)).willReturn(profile);
+        viewProfile = viewProfileImporter.importViewProfile(initials);
+    }
+
+    @Test
+    public void profileIsImportedAndSavedWithoutException() throws IOException {
+        addTestData();
+        ViewProfile retrieved = viewProfileService.getByIdAndCheckOwner(viewProfile.getId(), initials);
+        assertThat(retrieved).isNotNull();
+    }
+}
